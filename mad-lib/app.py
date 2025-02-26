8c04eebe111ef7412761dd2c856ef6384542e57e
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2025-02-26 22:54:25 +0500

    mad lib game done

---
+++ mad-lib/app.py
@@ -0,0 +1,9 @@
+print("Welcome to Mad lib game")
+
+name = input("Enter your name:")
+place = input("Enter your place:")
+car = input("Enter your car:")
+food = input("Enter your food:")
+
+story = (f"one day i went on picnic with my friend {name}. We will go to the {place} and have too much fun in his luxury car which name is {car} and then we eat {food}")
+print(story)
